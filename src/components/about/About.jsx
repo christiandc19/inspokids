2e1d2e83e7392daab8b150443c4fdf5d9382ce4d
unknown <christiandc19@hotmail.com>
2022-05-22 00:54:06 -0700

    added bootstrap and react fade package

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import { Fade } from "react-awesome-reveal";
+
 
 function About() {
   return (
+      <Fade>
     <div>
       <h1>About</h1>
     </div>
+    </Fade>
   )
 }
 
