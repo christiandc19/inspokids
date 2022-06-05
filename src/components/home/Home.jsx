4afa6586ac2bb5aa9ea8e92b8031da649b452721
Chris <christiandc19@Chriss-MBP.lan>
2022-06-05 00:52:00 -0700

    added header logo in Home Page

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import Logo from '../../assets/Logo.png'
+import './home.css'
 
 function Home() {
   return (
     <div>
-      <h1>Home</h1>
+      <div className='container logo-container'>
+      <h1>Welcome!</h1>
+      <img src={Logo} alt="" />
+      </div>
+
+
     </div>
   )
 }
