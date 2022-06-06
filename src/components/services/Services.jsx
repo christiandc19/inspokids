6779ac15884b0abb700939227106cc61be327ae4
Chris <christiandc19@Chriss-MBP.lan>
2022-06-05 20:37:03 -0700

    added react reveal annimation

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,11 +1,35 @@
 import React from 'react'
+import Fade from 'react-reveal/Fade';
+import './services.css';
 
-function Services() {
-  return (
-    <div>
-      <h1>Services</h1>
-    </div>
-  )
+
+class Services extends React.Component {
+  render() {
+    return (
+
+      <div>
+          <h1>React Reveal</h1>
+
+          <div className='container services-container'>
+
+            <Fade left>
+                <div className='service1'>
+                  <h1>Occupational Theraphy</h1>
+                </div>
+            </Fade>
+
+            <Fade right>
+                <div className='service-img1'>
+                  <h1>Image1</h1>
+                </div>
+            </Fade>
+
+          </div>
+      </div>
+
+    );
+  }
 }
+
 
 export default Services
